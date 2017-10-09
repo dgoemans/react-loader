aa0f2c3d2f52d60c97c64d822995ca86f67b5dab
David Goemans <dgoemans@gmail.com>
2017-10-09 20:11:59 +0200

    Basic react/webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require('path');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+
+var BUILD_DIR = path.resolve(__dirname, 'build/');
+var APP_DIR = path.resolve(__dirname, 'src/');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+        host: process.env.IP || 'localhost',
+        port: process.env.PORT || 3000,
+        server: {
+            baseDir: ['./', './build']
+        },
+        notify: false,
+        logLevel: 'debug'
+    })
+],
+  module : {
+    rules : [
+      {
+        test : /\.js?/,
+        include : APP_DIR,
+        use : 'babel-loader'
+      }
+    ]
+  },
+  watch: true
+};
+
+module.exports = config;
